21d01dbb58781b4c537c39ccedd5766e250a6065
irah rosete <irah.rosete@outlook.com>
2021-01-08 00:33:22 +1100

    fixed script and aaded styles to form

--- 08-form-wave-animation/script.js
+++ 08-form-wave-animation/script.js
@@ -2,7 +2,7 @@ const labels = document.querySelectorAll(".form-control label");
 
 labels.forEach(label => {
   label.innerHTML = label.innerText
-    .split("");
-    .map ((letter, idx)) => "<span>${letter}</span>;
+    .split("")
+    .map((letter, idx) => `<span>${letter}</span>`)
     .join("");
 })
